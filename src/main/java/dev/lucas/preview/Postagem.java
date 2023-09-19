5845894265c3ddc83ae52e409d7fa2784401a46e
Lucas Santiago Santana <lucin189@gmail.com>
2023-09-19 07:35:14 -0300

    feat(postagem): create constructors, getters and toString method for Postagem

--- src/main/java/dev/lucas/preview/Postagem.java
+++ src/main/java/dev/lucas/preview/Postagem.java
@@ -1,4 +1,41 @@
 package dev.lucas.preview;
 
+import java.util.Date;
+
 public sealed abstract class Postagem permits Elogio, Reclamacao {
+
+    private String titulo;
+    private String mensagem;
+    private Date criadoEm;
+
+    public Postagem() {
+        this.criadoEm = new Date();
+    }
+
+    public Postagem(String titulo, String mensagem) {
+        this.titulo = titulo;
+        this.mensagem = mensagem;
+        this.criadoEm = new Date();
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getMensagem() {
+        return mensagem;
+    }
+
+    public Date getCriadoEm() {
+        return criadoEm;
+    }
+
+    @Override
+    public String toString() {
+        return "Postagem{" +
+                "titulo='" + titulo + '\'' +
+                ", mensagem='" + mensagem + '\'' +
+                ", criadoEm=" + criadoEm +
+                '}';
+    }
 }
