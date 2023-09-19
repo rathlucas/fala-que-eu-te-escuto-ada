5709feb1d57e61b78fbc3e209df752012eb7657c
Lucas Santiago Santana <lucin189@gmail.com>
2023-09-19 07:38:58 -0300

    feat(app): create simple app demo

--- src/main/java/dev/lucas/preview/App.java
+++ src/main/java/dev/lucas/preview/App.java
@@ -5,14 +5,20 @@ public class App {
 
         Cliente cliente = new Cliente();
 
+        cliente.criarElogio("Otimo atendimento!", "Foram muito atenciosos");
+        System.out.println(cliente.getPostagens());
+
+        cliente.removerPostagem();
+        System.out.println(cliente.getPostagens());
     }
 }
 
 /*
 
-Altere a modelagem para limitar os filhos possíveis de Postagem e Usuário
-Implemente a pilha de postagens para o Cliente
-Implemente uma timeline com as postagens como lista ligada
-Altere a modelagem para adicionar a funcionalidade de “curtida” numa postagem de modo que o seja possível ordenar postagens com muito engajamento no topo
+[x] Altere a modelagem para limitar os filhos possíveis de Postagem e Usuário
+[] Implemente a pilha de postagens para o Cliente
+[] Implemente uma timeline com as postagens como lista ligada
+[] Altere a modelagem para adicionar a funcionalidade de “curtida” numa postagem de modo
+ que o seja possível ordenar postagens com muito engajamento no topo
 
  */
