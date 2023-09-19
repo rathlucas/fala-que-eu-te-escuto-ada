4c4cbaa27bbeac34f2c3cc3f4c48e77df41bef86
Lucas Santiago Santana <lucin189@gmail.com>
2023-09-19 11:47:09 -0300

    feat(postagem): implement Comparable interface and create curtir method

--- src/main/java/dev/lucas/preview/Postagem.java
+++ src/main/java/dev/lucas/preview/Postagem.java
@@ -1,13 +1,16 @@
 package dev.lucas.preview;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
-public sealed abstract class Postagem permits Elogio, Reclamacao {
+public sealed abstract class Postagem implements Comparable<Postagem> permits Elogio, Reclamacao  {
 
     private String titulo;
     private String mensagem;
     private Empresa empresa;
     private Date criadoEm;
+    private final List<Curtida> curtidas = new ArrayList<>();
 
     public Postagem() {
         this.criadoEm = new Date();
@@ -36,12 +39,23 @@ public sealed abstract class Postagem permits Elogio, Reclamacao {
         return criadoEm;
     }
 
+    public List<Curtida> getCurtidas() {
+        return curtidas;
+    }
+
+    public void curtir(Curtida curtida) {
+        curtidas.add(curtida);
+        System.out.println("Curtida adicionada com sucesso!");
+    }
+
     @Override
     public String toString() {
         return "Postagem{" +
                 "titulo='" + titulo + '\'' +
                 ", mensagem='" + mensagem + '\'' +
+                ", empresa=" + empresa +
                 ", criadoEm=" + criadoEm +
+                ", curtidas=" + curtidas.size() +
                 '}';
     }
 }
