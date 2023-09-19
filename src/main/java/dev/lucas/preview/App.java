247219473c235ec4a15340e923668e29d383396b
Lucas Santiago Santana <lucin189@gmail.com>
2023-09-19 11:52:21 -0300

    feat(app): add code demo of Curtidas and Timeline systems

--- src/main/java/dev/lucas/preview/App.java
+++ src/main/java/dev/lucas/preview/App.java
@@ -25,6 +25,27 @@ public class App {
         for (var postagem : timeline.getPostagens()) {
             System.out.println(postagem);
         }
+
+        var elogio1 = cliente.criarElogio("Gostei!",
+                "Foram muito atenciosos", empresa);
+        elogio1.curtir(new Curtida());
+
+        timeline.adicionarPostagem(elogio1);
+
+        var elogio2 = cliente.criarElogio("Daora o atendimento!",
+                "Foram muito bacanas", empresa);
+        elogio2.curtir(new Curtida("Top!"));
+        elogio2.curtir(new Curtida());
+        elogio2.curtir(new Curtida());
+
+        timeline.adicionarPostagem(elogio2);
+
+        var elogio3 = cliente.criarElogio("Otimo atendimento!",
+                "Foram muito atenciosos", empresa);
+
+        timeline.adicionarPostagem(elogio3);
+
+        System.out.println(timeline.getPostagensMaisCurtidas());
     }
 }
 
@@ -33,7 +54,7 @@ public class App {
 [x] Altere a modelagem para limitar os filhos possíveis de Postagem e Usuário
 [x] Implemente a pilha de postagens para o Cliente
 [x] Implemente uma timeline com as postagens como lista ligada
-[] Altere a modelagem para adicionar a funcionalidade de “curtida” numa postagem de modo
+[x] Altere a modelagem para adicionar a funcionalidade de “curtida” numa postagem de modo
  que o seja possível ordenar postagens com muito engajamento no topo
 
  */
