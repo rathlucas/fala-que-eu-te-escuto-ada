5f32f454426d3a2d0926a55f9af84be074c3f8d1
Lucas Santiago Santana <lucin189@gmail.com>
2023-09-19 11:47:50 -0300

    feat(curtida): implement class body

--- src/main/java/dev/lucas/preview/Curtida.java
+++ src/main/java/dev/lucas/preview/Curtida.java
@@ -1,4 +1,26 @@
 package dev.lucas.preview;
 
+import java.util.Date;
+
 public class Curtida {
+
+    private String comentario;
+    private final Date criadoEm;
+
+    public Curtida() {
+        this.criadoEm = new Date();
+    }
+
+    public Curtida(String comentario) {
+        this.comentario = comentario;
+        this.criadoEm = new Date();
+    }
+
+    @Override
+    public String toString() {
+        return "Curtida{" +
+                "comentario='" + comentario + '\'' +
+                ", criadoEm=" + criadoEm +
+                '}';
+    }
 }
