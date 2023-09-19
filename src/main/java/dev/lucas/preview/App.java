13c9f9e6da0dce7a3b1652fd2e1378373222ec61
Lucas Santiago Santana <lucin189@gmail.com>
2023-09-19 08:05:56 -0300

    feat(app): add more cases to demo the Timeline

--- src/main/java/dev/lucas/preview/App.java
+++ src/main/java/dev/lucas/preview/App.java
@@ -3,21 +3,35 @@ package dev.lucas.preview;
 public class App {
     public static void main(String[] args) {
 
+        Timeline timeline = new Timeline();
         Cliente cliente = new Cliente();
 
-        cliente.criarElogio("Otimo atendimento!", "Foram muito atenciosos");
+        timeline.adicionarPostagem(cliente.criarElogio("Otimo atendimento!",
+                "Foram muito atenciosos"));
         System.out.println(cliente.getPostagens());
+        System.out.println(timeline);
 
-        cliente.removerPostagem();
+
+        timeline.removerPostagem(cliente.removerPostagem());
         System.out.println(cliente.getPostagens());
+        System.out.println(timeline);
+
+        timeline.adicionarPostagem(cliente.criarReclamacao("Pessimo!",
+                "Odiei o serviço"));
+        System.out.println(cliente.getPostagens());
+        System.out.println(timeline);
+
+        for (var postagem : timeline.getPostagens()) {
+            System.out.println(postagem);
+        }
     }
 }
 
 /*
 
 [x] Altere a modelagem para limitar os filhos possíveis de Postagem e Usuário
-[] Implemente a pilha de postagens para o Cliente
-[] Implemente uma timeline com as postagens como lista ligada
+[x] Implemente a pilha de postagens para o Cliente
+[x] Implemente uma timeline com as postagens como lista ligada
 [] Altere a modelagem para adicionar a funcionalidade de “curtida” numa postagem de modo
  que o seja possível ordenar postagens com muito engajamento no topo
 
