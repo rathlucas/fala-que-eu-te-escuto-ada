192ee7b117dde84398391b752f190291a3f224ee
Lucas Santiago Santana <lucin189@gmail.com>
2023-09-19 07:35:54 -0300

    feat(elogio,reclamacao): create constructors

--- src/main/java/dev/lucas/preview/Elogio.java
+++ src/main/java/dev/lucas/preview/Elogio.java
@@ -2,4 +2,9 @@ package dev.lucas.preview;
 
 public final class Elogio extends Postagem {
 
+    public Elogio() {}
+
+    public Elogio(String titulo, String mensagem) {
+        super(titulo, mensagem);
+    }
 }
--- src/main/java/dev/lucas/preview/Reclamacao.java
+++ src/main/java/dev/lucas/preview/Reclamacao.java
@@ -1,4 +1,10 @@
 package dev.lucas.preview;
 
 public final class Reclamacao extends Postagem {
+
+    public Reclamacao() {}
+
+    public Reclamacao(String titulo, String mensagem) {
+        super(titulo, mensagem);
+    }
 }
