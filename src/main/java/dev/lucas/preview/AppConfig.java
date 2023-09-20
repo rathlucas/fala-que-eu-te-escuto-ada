30e09dc9c41c9ec67c81f298c30cad5d7deffab6
Lucas Santiago Santana <lucin189@gmail.com>
2023-09-20 00:10:11 -0300

    feat(app-config): create AppConfig and declare modelMapper method

--- src/main/java/dev/lucas/preview/AppConfig.java
+++ src/main/java/dev/lucas/preview/AppConfig.java
@@ -1,2 +1,14 @@
-package dev.lucas.preview;public class AppConfig {
+package dev.lucas.preview;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
 }
