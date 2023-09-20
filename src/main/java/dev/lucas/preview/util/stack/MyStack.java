0a8071c48b557b2c1d136e63a5d74390ce43e88c
Lucas Santiago Santana <lucin189@gmail.com>
2023-09-20 00:04:29 -0300

    feat(util): create util package with Queue and Stack definitions to implement

--- src/main/java/dev/lucas/preview/util/stack/MyStack.java
+++ src/main/java/dev/lucas/preview/util/stack/MyStack.java
@@ -1,4 +1,28 @@
-package dev.lucas.preview.util.queue.stack;
+package dev.lucas.preview.util.stack;
 
-public class Stack {
+public class MyStack<T> implements Stack<T> {
+    @Override
+    public T push(T element) {
+        return null;
+    }
+
+    @Override
+    public T pop() {
+        return null;
+    }
+
+    @Override
+    public T top() {
+        return null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
 }
--- src/main/java/dev/lucas/preview/util/stack/Stack.java
+++ src/main/java/dev/lucas/preview/util/stack/Stack.java
@@ -1,2 +1,14 @@
-package dev.lucas.preview.util.stack;public interface Stack {
+package dev.lucas.preview.util.stack;
+
+public interface Stack<T> {
+
+    T push(T element);
+
+    T pop();
+
+    T top();
+
+    boolean isEmpty();
+
+    int size();
 }
