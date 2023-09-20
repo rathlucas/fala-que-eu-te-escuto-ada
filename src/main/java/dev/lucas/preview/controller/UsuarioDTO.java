7376fc70054bb36f4b9088bfee92aa6210445653
Lucas Santiago Santana <lucin189@gmail.com>
2023-09-20 00:11:14 -0300

    feat(usuario-dto): create dto

--- src/main/java/dev/lucas/preview/controller/UsuarioDTO.java
+++ src/main/java/dev/lucas/preview/controller/UsuarioDTO.java
@@ -1,2 +1,26 @@
-package dev.lucas.preview.controller;public class UsuarioDTO {
+package dev.lucas.preview.controller;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.TimeZone;
+
+public class UsuarioDTO {
+
+    private static final SimpleDateFormat formatadorDeData
+            = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+
+    private String nome;
+    private String email;
+    private LocalDate dataNascimento;
+    private String criadoEm;
+
+    public java.util.Date getCriadoEmConvertido(String timezone) throws java.text.ParseException {
+        formatadorDeData.setTimeZone(TimeZone.getTimeZone(timezone));
+        return formatadorDeData.parse(this.criadoEm);
+    }
+
+    public void setCriadoEm(java.util.Date criadoEm, String timezone) {
+        formatadorDeData.setTimeZone(TimeZone.getTimeZone(timezone));
+        this.criadoEm = formatadorDeData.format(criadoEm);
+    }
 }
