7c071dcc94856159d2bb1b00594aa3e5e9ea299d
Lucas Santiago Santana <lucin189@gmail.com>
2023-09-20 00:10:52 -0300

    feat(postagem-controller): create controller

--- src/main/java/dev/lucas/preview/controller/PostagemController.java
+++ src/main/java/dev/lucas/preview/controller/PostagemController.java
@@ -1,2 +1,34 @@
-package dev.lucas.preview.controller;public class PostagemController {
+package dev.lucas.preview.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/postagem")
+public class PostagemController {
+
+    @GetMapping
+    public String olaMundo() {
+        return "Ola, mundo!";
+    }
+
+    @PostMapping(value = "/cadastro")
+    public String olaMundoPost(@RequestBody UsuarioDTO usuario) {
+        return "Ola, post";
+    }
+
+    @PatchMapping(value = "/atualizar/{id}")
+    public String olaMundoPatch(@PathVariable("id") int id) {
+        return "Ola, patch";
+    }
+
+    @PutMapping
+    public String olaMundoPut() {
+        return "Ola, put";
+    }
+
+    @DeleteMapping
+    public String olaMundoDelete() {
+        return "Ola, delete";
+    }
+
 }
