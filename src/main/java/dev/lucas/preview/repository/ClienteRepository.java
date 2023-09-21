cf0188afb73645f2ac0a8bb801924c62d9fdb98f
Lucas Santiago Santana <lucin189@gmail.com>
2023-09-21 08:20:00 -0300

    feat(repository): add custom findById declaration

--- src/main/java/dev/lucas/preview/repository/ClienteRepository.java
+++ src/main/java/dev/lucas/preview/repository/ClienteRepository.java
@@ -4,6 +4,10 @@ import dev.lucas.preview.model.cadastro.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+import java.util.UUID;
+
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+    Optional<Cliente> findById(UUID id);
 }
--- src/main/java/dev/lucas/preview/repository/EmpresaRepository.java
+++ src/main/java/dev/lucas/preview/repository/EmpresaRepository.java
@@ -3,5 +3,10 @@ package dev.lucas.preview.repository;
 import dev.lucas.preview.model.cadastro.Empresa;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+import java.util.UUID;
+
 public interface EmpresaRepository extends JpaRepository<Empresa, Integer> {
+
+    Optional<Empresa> findById(UUID id);
 }
