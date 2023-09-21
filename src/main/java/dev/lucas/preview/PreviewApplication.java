81be2ffc872a8f6a5be1d634ea89deea20d31206
Lucas Santiago Santana <lucin189@gmail.com>
2023-09-21 00:47:01 -0300

    feat(preview-application): add modelMapper @Bean

--- src/main/java/dev/lucas/preview/PreviewApplication.java
+++ src/main/java/dev/lucas/preview/PreviewApplication.java
@@ -1,7 +1,10 @@
 package dev.lucas.preview;
 
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class PreviewApplication {
@@ -10,4 +13,10 @@ public class PreviewApplication {
 		SpringApplication.run(PreviewApplication.class, args);
 	}
 
+	@Bean
+	public ModelMapper modelMapper() {
+		ModelMapper modelMapper = new ModelMapper();
+		modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+		return modelMapper;
+	}
 }
