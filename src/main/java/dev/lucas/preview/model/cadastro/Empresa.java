8ae2fba8c8538557d1d3f83c02a0eac0a6aee45b
Lucas Santiago Santana <lucin189@gmail.com>
2023-09-21 07:53:48 -0300

    refactor(empresa): make id UUID type

--- src/main/java/dev/lucas/preview/model/cadastro/Empresa.java
+++ src/main/java/dev/lucas/preview/model/cadastro/Empresa.java
@@ -2,30 +2,41 @@ package dev.lucas.preview.model.cadastro;
 
 import dev.lucas.preview.model.postagem.Postagem;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 @Entity
 public final class Empresa extends Usuario {
 
+    @OneToMany
+    private final List<Postagem> postagens = new ArrayList<>();
+
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Integer id;
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
 
     @Pattern(regexp = "(^\\d{2}.\\d{3}.\\d{3}/\\d{4}-\\d{2}$)")
     @Column(nullable = false)
     private String cnpj;
 
+    @Size(min = 4, max = 50)
     @Column(nullable = false)
     private String nomeFantasia;
 
-    @OneToMany
-    private final List<Postagem> postagens = new ArrayList<>();
+    @Email
+    @Column(nullable = false)
+    private String email;
+
+    @Column
+    private String area;
 
     @CreationTimestamp
     private Instant criadoEm;
@@ -33,12 +44,16 @@ public final class Empresa extends Usuario {
     @UpdateTimestamp
     private Instant atualizadoEm;
 
-    public Empresa(){
+    public Empresa() {
     }
 
     public Empresa(String cnpj, String nomeFantasia) {
         this.cnpj = cnpj;
         this.nomeFantasia = nomeFantasia;
+    }
+
+    public UUID getId() {
+        return id;
     }
 
     public String getCnpj() {
@@ -59,6 +74,14 @@ public final class Empresa extends Usuario {
 
     public List<Postagem> getPostagens() {
         return postagens;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public Instant getCriadoEm() {
